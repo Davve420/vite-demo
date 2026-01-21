2f7861a8afd51b94ce84f9267a1629d037bf0092
Davve420 <davidbrolin04@gmail.com>
2026-01-21 11:59:14 +0100

    ny bild och animation

--- src/main.js
+++ src/main.js
@@ -1,15 +1,7 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { animate } from "animejs";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <h1>Counter app, with Vite</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+animate('h1',{
+  borderRadius: 64,
+  'background-color': '#F9F640',
+  loop: true
+});
